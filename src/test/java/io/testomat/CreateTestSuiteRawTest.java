fee4d6bb28210914e0a83aebedb2323715fac576
Roman Marinsky <newromka@gmail.com>
2023-09-11 20:49:50 +0300

    Initial commit

---
+++ src/test/java/io/testomat/CreateTestSuiteRawTest.java
@@ -0,0 +1,66 @@
+package io.testomat;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class CreateTestSuiteRawTest {
+
+    Faker faker = new Faker();
+
+
+    static {
+        //        Configuration.timeout
+    }
+
+    @Test
+    @DisplayName("Should be possible to create test suite for new project")
+    void shouldBePossibleToCreateTestSuiteForNewProject() {
+        Selenide.open("https://app.testomat.io/users/sign_in");
+        loginUser("newromka@gmail.com", "p8qfCZ7Jv7pT!hh");
+
+        preloaderIsHidden();
+        $("#content-desktop h2").shouldBe(Condition.visible);
+        $("#content-desktop [href='/projects/new']").click();
+
+        preloaderIsHidden();
+        $("#project-form #project_title").setValue(faker.commerce().department());
+        $("[name='commit']").click();
+
+        preloaderIsHidden();
+        $("[placeholder='First Suite']").shouldBe(Condition.visible);
+        $(".back").click();
+
+        String targetTestSuite = faker.commerce().productName();
+
+        $("[placeholder='First Suite']")
+                .setValue(targetTestSuite)
+                .pressEnter();
+
+        $$(".list-group-wrapper .dragSortItem").shouldHave(size(1));
+        $(".list-group-wrapper .dragSortList").shouldHave(text(targetTestSuite));
+    }
+
+    //carete
+
+    private static void loginUser(String mail, String password) {
+        $("#content-desktop #user_email").setValue(mail);
+        $("#content-desktop #user_password").setValue(password);
+        $("#content-desktop [name='commit']").click();
+    }
+
+    private void preloaderIsHidden() {
+        $("#app-loader").shouldBe(Condition.disappear, Duration.ofSeconds(30));
+    }
+
+}
