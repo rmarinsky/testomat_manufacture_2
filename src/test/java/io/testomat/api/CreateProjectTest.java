f2f4e2675cd0ed7b96dedb16aab97322f28663e3
Roman Marinsky <newromka@gmail.com>
2023-09-28 21:03:38 +0300

    lesson 5, 28.09.2023

--- src/test/java/io/testomat/api/CreateProjectTest.java
+++ src/test/java/io/testomat/api/CreateProjectTest.java
@@ -37,7 +37,6 @@ public class CreateProjectTest {
                 .prettyPeek();
 */
 
-        //&project%5Blang%5D=js&project%5Btitle%5D=Popopo&project%5Burl%5D=&commit=Create
         testomatSpecification
                 .header("Authorization", token)
                 .get("/{projectName}/suites", projectname)
